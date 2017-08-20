d96c283a5bd1f161543709ad46d996927938ec8a
Yasuyuki Kamata <yasuyuki@unity3d.com>
2017-08-20 14:51:50 +0900

    first commit

---
+++ Assets/MainController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+	public Animator anim;
+
+	public void OnClick (string obj)
+	{
+		Debug.Log ("click: " + obj);
+		anim.SetTrigger (obj);
+	}
+}
